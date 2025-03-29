7d53ffeffe24eec66a08f06c8f9b2dbab81dbff5
Yuranium <dolazifet@gmail.com>
2025-03-30 00:50:39 +0300

    Adding new React component - EditTaskForm

---
+++ react_application/src/components/task-form/edit-task-form/edit-task-form.jsx
@@ -0,0 +1,6 @@
+export default function EditTaskForm(props)
+{
+    return (
+        <div></div> //todo поменять расположение компонентов, выделить edit and new task были на уровень ниже
+    )
+}
--- react_application/src/index.js
+++ react_application/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NewTaskForm from "./components/new-task-form/new-task-form";
+import NewTaskForm from "./components/task-form/new-task-form/new-task-form";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
