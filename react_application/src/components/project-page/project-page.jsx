139751e4f040e49dbb4b46478b86d3c5e62d8554
Yuranium <dolazifet@gmail.com>
2025-04-06 00:01:08 +0300

    Start setting up linking a project to a task list

--- react_application/src/components/project-page/project-page.jsx
+++ react_application/src/components/project-page/project-page.jsx
@@ -1,63 +1,126 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
+import {CiCirclePlus} from "react-icons/ci";
 import axios from "axios";
 import Button from "../button/button";
 import './project-page.css'
+import TaskCard from "../task-card/task-card";
 
-export default function ProjectPage() // todo сделать sidebar слева
-{
-    const { id } = useParams();
+export default function ProjectPage({projectId}) {
+    //const {id} = useParams();
     const navigate = useNavigate();
-    const [project, setProject] = useState(null);
+    const [tasks, setTasks] = useState(null);
     const [loading, setLoading] = useState(true);
+    const didFetch = useRef(false);
+    const [projectEmpty, setProjectEmpty] = useState(false);
+
+
+    // todo проверять на наличие данного проекта на бэке
+    const fetchTasks = async () => {
+        try {
+            const backHost = process.env.REACT_APP_BACKEND_PROJECT_SERVICE_HOST
+            const backPort = process.env.REACT_APP_BACKEND_PORT
+            const response = await axios.get(
+                `http://${backHost}:${backPort}/api/tasks/getAllTasks`, {
+                    params: {
+                        projectId: projectId
+                    }
+                });
+            setTasks(response.data);
+            console.log(response.data)
+        } catch (err) {
+            if (err.response?.status === 404)
+                navigate('/404');
+        } finally {
+            if (!tasks || tasks.length === 0)
+                setProjectEmpty(true)
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchProject = async () => {
-            try {
-                const backHost = process.env.REACT_APP_BACKEND_PROJECT_SERVICE_HOST
-                const backPort = process.env.REACT_APP_BACKEND_PORT
-                const response = await axios.get(
-                    `http://${backHost}:${backPort}/api/projects/${id}`);
-                setProject(response.data);
-                console.log(response.data)
-            } catch (err) {
-                navigate('/404');
-            } finally {
-                setLoading(false);
-            }
-        };
+        if (!didFetch.current) {
+            didFetch.current = true;
+            fetchTasks();
+        }
 
-        fetchProject();
-    }, [id, navigate]); // Повторный запрос при изменении ID
+        fetchTasks();
+    }, [navigate]);
 
     if (loading) return <div>Загрузка проекта...</div>;
-    if (!project) return <div>Проект не найден</div>;
+
+    if (projectEmpty)
+        return (
+            <div>
+                Данный проект на данный момент пустой
+            </div>
+        )
 
     return (
         <div className="project-page-main">
-            <div className="container">
-                <a className="anchor-project-card" href={`/projects/${project.id}`}>
-                    <div className="main-info">
-                        <img
-                            className="image-project-card"
-                            src={`data:${project.avatars[0].contentType};base64,${project.avatars[0].binaryData}`}
-                            alt={`Название: ${project.avatars[0].name}`}
-                            onError={(e) => {
-                                e.target.onerror = null;
-                                e.target.src = '/placeholder-image.jpg';
-                            }}
-                        />
-                        <p className="project-name">{project.name}</p>
-                    </div>
-                    <div className="container-project-card-info">
-                        <p>{project.description}</p>
-                        <p>{`Дата создания: ${project.dateAdded}`}</p>
-                    </div>
-                    <div className="buttons">
-                        <Button>Изменить</Button>
-                        <Button>Удалить</Button>
-                    </div>
-                </a>
+            <aside className="project-sidebar">
+                <h3>Проекты</h3>
+                <label htmlFor="project-search" className="project-page-label"></label>
+                <input
+                    type="text"
+                    placeholder="Найти проект"
+                    name="projectSearch"
+                    id="project-search"/>
+                <ul>
+                    <li>Проект №1</li>
+                    <li>Проект №2</li>
+                    <li>Проект №3</li>
+                </ul>
+            </aside>
+
+            <div className="project-page-main-1">
+                <nav className="task-navbar">
+                    <Button><CiCirclePlus /></Button>
+
+                    <label htmlFor="task-search" className="project-page-label"></label>
+                    <input
+                        type="text"
+                        placeholder="Найти задачу"
+                        name="projectSearch"
+                        id="task-search"/>
+
+                    <span className="task-status-filter">
+                        <label htmlFor="task-status-sort" className="project-page-label">
+                            Сортировка по статусу:</label>
+                        <select id="task-status-sort">
+                            <option value=""></option>
+                            <option value="PLANING">В планах</option>
+                            <option value="IN_PROGRESS">В процессе</option>
+                            <option value="COMPLETED">Завершена</option>
+                            <option value="CANCELED">Отменена</option>
+                            <option value="EXPIRED">Просрочена</option>
+                        </select>
+                    </span>
+
+                    <span className="task-importance-filter">
+                        <label htmlFor="task-importance-sort" className="project-page-label">
+                            Сортировка по важности:</label>
+                        <select id="task-importance-sort">
+                            <option value=""></option>
+                            <option value="LOW">Низкая</option>
+                            <option value="INTERMEDIATE">Средняя</option>
+                            <option value="HIGH">Высокая</option>
+                        </select>
+                    </span>
+
+                    <span className="task-finish-filter">
+                        <label htmlFor="task-is-finish" className="project-page-label">
+                            Задача завершена?</label>
+                        <input
+                            type="checkbox"
+                            id="task-is-finish"/>
+                    </span>
+                </nav>
+                {
+                    tasks.map(task => {
+                        return <TaskCard task={task} avatars={task.avatars} />
+                    })
+                }
             </div>
         </div>
     );
--- react_application/src/components/task-card/task-card.jsx
+++ react_application/src/components/task-card/task-card.jsx
@@ -1,26 +1,30 @@
 import './task-card.css'
 import Button from "../button/button";
+import {useNavigate} from "react-router-dom";
 
-export default function TaskCard({task}) {
+export default function TaskCard({task, avatars})
+{
+    const navigate = useNavigate()
     return (
-        <div className="main">
-            <div className="container">
-                <a className="anchor-task-card" href={`/tasks/${task.id}`}>
-                    <div className="main-info">
-                        <img className="image-task-card" src="../account/Кактус.png" alt="avatar"/>
-                        <p className="task-name">{task.name}</p>
-                    </div>
-                    <div className="container-task-card-info">
-                        <p>{task.description}</p>
-                        <p>{task.importance}</p>
-                        <p>{task.status}</p>
-                        <p>{task.dateFinish}</p>
-                    </div>
-                    <div className="buttons">
-                        <Button>Изменить</Button>
-                        <Button>Удалить</Button>
-                    </div>
-                </a>
+        <div className="container">
+            <div
+                className="anchor-task-card"
+                onClick={() => navigate(`/${task.id}`)}
+                role="button">
+                <div className="main-info">
+                    <img className="image-task-card" src="../account/Кактус.png" alt="task-avatar"/>
+                    <h3 className="task-name">{task.name}</h3>
+                </div>
+                <div className="container-task-card-info">
+                    <p>{task.description}</p>
+                    <p>{task.importance}</p>
+                    <p>{task.status}</p>
+                    <p>{task.dateFinish}</p>
+                </div>
+                <div className="task-card-buttons">
+                    <Button>Изменить</Button>
+                    <Button>Удалить</Button>
+                </div>
             </div>
         </div>
     )
